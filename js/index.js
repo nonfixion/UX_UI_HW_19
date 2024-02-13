c6b949ab93167b302adad44a1311f88ddda98912
nonfixion <megsferry@gmail.com>
2024-02-12 21:35:01 -0500

    updated js

--- js/index.js
+++ js/index.js
@@ -1,14 +1,6 @@
 console.log("Your index.js is loaded correctly!");
 
-$("a[href='#top']").click(function() {
-    $("html, body").animate({ scrollTop: 100 }, "slow");
+$("#backToTopImg").click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
-});
-
-$('backToTopImg').hover(
-function() {
-    $(this).attr('src', "images\top hover.png");
-},
-function() {
-    $(this).attr('src', '');
 });
